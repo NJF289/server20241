7d6892d2f3b57c82a108d24d74cc6425acdace65
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-06-05 00:47:30 +0000

    feat: cadastro estudante

---
+++ controllers/studant.ts
@@ -0,0 +1,23 @@
+import { pool } from "../shared/database";
+import { Request, Response, Router } from "express";
+
+
+export async function listStudants(req: Request, res: Response) {
+  //conecta com o banco
+  const client = await pool.connect();
+  //realiza consulta sql
+  const studants = await client.query(`select * from studants`)
+  //retorna consulta em formato json
+  return res.status(200).json(studants.rows);
+}
+export async function saveStudants(req: Request, res: Response) {
+  const studants = req.body;
+  //conecta com o banco
+  const client = await pool.connect();
+  //realiza consulta sql
+  const response = await client.query(`INSERT INTO courses (name,email) VALUES ('${studants.name,studants.email}')`)
+  res.status(201).json(response.rows);
+}
+
+
+
--- router.ts
+++ router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { listCourse, saveCourse } from "./controllers/course";
+import { listStudants, saveStudants } from "./controllers/studant";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse)
+router.get("/studants", listStudants);
+router.post("/studants", saveStudants)
 
 export { router };
