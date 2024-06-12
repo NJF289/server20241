ade18b05df5dbb6258b4c4eef7524d16b83237b4
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-06-12 01:21:11 +0000

    ajustes

--- controllers/studant.ts
+++ controllers/studant.ts
@@ -2,18 +2,7 @@ import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
 
-export async function  deleteStudants (req: Request, res: Response) {
-  const client = await pool.connect();
-  const id = req.params.id
-  try {
-    const response = await client.query('delete from studants where id=$(id)');
-    res.status(200).json("mensagem: registro excluio")
-  } catch (error) {
-    res.status(404).json("mensagem: ERROR")
-  }finally{
-    client.release()
-  }
-}
+
 
 export async function listStudants(req: Request, res: Response) {
   //conecta com o banco
