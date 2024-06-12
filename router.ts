becbfcdc4cedc2e4828f85ed6abd3a98d7f97d1e
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-06-12 00:05:55 +0000

    Delete

--- router.ts
+++ router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { listCourse, saveCourse } from "./controllers/course";
-import { listStudants, saveStudants } from "./controllers/studant";
+import { listStudants, saveStudants, deleteStudants } from "./controllers/studant";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse);
 router.get("/studants", listStudants);
-router.post("/studants", saveStudants)
-
+router.post("/studants", saveStudants);
+router.delete("/studants/:id", deleteStudants)
 export { router };
