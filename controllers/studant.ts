2b5125c20317662c644ad610ab2ae4394437091e
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-06-13 13:09:05 +0000

    Fix:try

--- controllers/studant.ts
+++ controllers/studant.ts
@@ -2,25 +2,48 @@ import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
 
+export async function  deleteStudants (req: Request, res: Response) {
+  const client = await pool.connect();
+  const id = req.params.id
+  try {
+    const response = await client.query('delete from studants where id=$(id)');
+    res.status(200).json("mensagem: registro excluio")
+  } catch (error) {
+    res.status(404).json("mensagem: ERROR")
+  }finally{
+    client.release()
+  }
+}
 
 
 export async function listStudants(req: Request, res: Response) {
-  //conecta com o banco
   const client = await pool.connect();
-  //realiza consulta sql
-  const studants = await client.query(`select * from studants`)
-  //retorna consulta em formato json
-  return res.status(200).json(studants.rows);
-}
-export async function saveStudants(req: Request, res: Response) {
-  const studants = req.body;
-  console.log(studants)
-  //conecta com o banco
-  const client = await pool.connect();
-  //realiza consulta sql
-  const response = await client.query(`INSERT INTO studants (name, email) VALUES ('${studants.name}','${studants.email}')`)
-  res.status(201).json(response.rows);
+  try {
+    const studants = await client.query(`select * from studants`)
+    if (studants.rowCount === 0) {
+      return res.status(404).json({ message: "não encontrado" });
+    }
+    return res.status(200).json(studants.rows);
+  }catch(error){
+    console.log(error)
+  }finally{
+    client.release;
+  }
 }
 
-
-
+export async function saveStudent(req: Request, res: Response) {
+  const client = await pool.connect();
+  const student = req.body;
+  console.log(student);
+  try {
+    const response = await client.query(
+      `insert INTO studants (name, email) VALUES ('${student.name}','${student.email}' ) RETURNING *`,
+    );
+    console.log(response.rows[0]);
+    res.status(201).json(response.rows[0]);
+  } catch (error) {
+    res.status(400).json({ message: 'Dados inválidos:', error});
+  } finally {
+    client.release();
+  }
+}
