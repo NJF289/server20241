3b3020c6f6a26b74860053df8c12dd2da51aa154
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-06-13 19:36:58 +0000

    fix: course try

--- controllers/course.ts
+++ controllers/course.ts
@@ -2,23 +2,49 @@ import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
 
-export async function listCourse(req: Request, res: Response) {
-  //conecta com o banco
+
+
+export async function  deleteCourse (req: Request, res: Response) {
   const client = await pool.connect();
-  //realiza consulta sql
-  const courses = await client.query(`select * from courses`)
-  //retorna consulta em formato json
-  return res.status(200).json(courses.rows);
-}
-export async function saveCourse(req: Request, res: Response) {
-  const course = req.body;
-  console.log(course)
-  //conecta com o banco
-  const client = await pool.connect();
-  //realiza consulta sql
-  const response = await client.query(`INSERT INTO courses (name) VALUES ('${course.name}')`)
-  res.status(201).json(response.rows);
+  const id = req.params.id
+  try {
+    const response = await client.query('delete from studants where id=$(id)');
+    res.status(200).json("mensagem: registro excluio")
+  } catch (error) {
+    res.status(404).json("mensagem: ERROR")
+  }finally{
+    client.release()
+  }
 }
 
+export async function listStudants(req: Request, res: Response) {
+  const client = await pool.connect();
+  try {
+    const courses = await client.query(`select * from courses`)
+    if (courses.rowCount === 0) {
+      return res.status(404).json({ message: "não encontrado" });
+    }
+    return res.status(200).json(courses.rows);
+  }catch(error){
+    console.log(error)
+  }finally{
+    client.release;
+  }
+}
 
-
+export async function saveStudants(req: Request, res: Response) {
+  const client = await pool.connect();
+  const courses = req.body;
+  console.log(courses);
+  try {
+    const response = await client.query(
+      `INSERT INTO courses (name) VALUES ('${courses.name}') RETURNING *`,
+    );
+    console.log(response.rows[0]);
+    res.status(201).json(response.rows[0]);
+  } catch (error) {
+    res.status(400).json({ message: 'Dados inválidos:', error});
+  } finally {
+    client.release();
+  }
+}
